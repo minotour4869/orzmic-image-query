10e778652a4ba0068cf19fc8c18095665f0fbdd2
Trần Quốc Lân <62930566+minotour4869@users.noreply.github.com>
2025-01-30 01:28:16 +0700

    chore: remove orz/ env directory (somehow it's still there)

--- orz/lib/python3.9/site-packages/UnityPy/classes/__init__.py
+++
@@ -1,5 +0,0 @@
-from .generated import *
-from .legacy_patch import *
-from .Object import Object as Object
-from .PPtr import PPtr as PPtr
-from .UnknownObject import UnknownObject as UnknownObject
